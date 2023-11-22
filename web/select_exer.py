ab17fe1b542b1e43e672fd6ef77087090e207fbe
Yunsoonsang <45776940+Yunsoonsang@users.noreply.github.com>
2023-11-23 04:45:49 +0900

    Delete web directory

--- web/select_exer.py
+++
@@ -1,8 +0,0 @@
-class ExerciseData:
-    def __init__(self, exercises):
-        self.exercises = exercises
-
-    def to_dict(self):
-        return {
-            'exercises': self.exercises
-        }
